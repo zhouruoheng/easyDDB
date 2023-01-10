d871f3612b187c8fd6b6da7c4a56e4215bb13f40
zhouyanruc <zywnhc@outlook.com>
2023-01-10 15:14:26 +0000

    data

--- ddbs/client/src/client.cpp
+++ ddbs/client/src/client.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <thread>
 #include <gflags/gflags.h>
 #include <butil/logging.h>
@@ -6,18 +8,21 @@
 #include <brpc/channel.h>
 #include <client/cli.hpp>
 #include <db.pb.h>
+#include <nlohmann/json.hpp>
 
 // test use begin
 #include <SQLParser.h>
 #include <util/sqlhelper.h>
 // end
 
+using json = nlohmann::json;
+
 DEFINE_string(attachment, "", "Carry this along with requests");
 DEFINE_string(protocol, "baidu_std", "Protocol type. Defined in src/brpc/options.proto");
 DEFINE_string(connection_type, "", "Connection type. Available values: single, pooled, short");
 DEFINE_string(server, "localhost:1231", "IP Address of server");
 DEFINE_string(load_balancer, "", "The algorithm for load balancing");
-DEFINE_int32(timeout_ms, 200000, "RPC timeout in milliseconds");
+DEFINE_int32(timeout_ms, 10000, "RPC timeout in milliseconds");
 DEFINE_int32(max_retry, 3, "Max retries(not including the first RPC)");
 
 void test_fn(const std::string &query) {
@@ -43,6 +48,15 @@ void test_fn(const std::string &query) {
             for (auto &column: *create->columns) {
                 std::cout << "(" << column->name << "," << column->type << ")" << std::endl;
             }
+        }
+        else if (statement->isType(hsql::kStmtDelete)) {
+            const auto* del = static_cast<const hsql::DeleteStatement*>(statement);
+            if (del->schema != nullptr)
+                std::cout << "schema: " << del->schema << std::endl;
+            if (del->tableName != nullptr)
+                std::cout << "tableName: " << del->tableName << std::endl;
+            if (del->expr != nullptr)
+                printExpression(del->expr, 1);
         }
     }
 }
@@ -78,7 +92,8 @@ std::string send_request(
     } else {
         ss << cntl.ErrorText();
     }
-    return ss.str();
+    std::cout << ss.str() << std::endl;
+    return response.msg();
 }
 
 int main(int argc, char *argv[]) {
@@ -107,9 +122,13 @@ int main(int argc, char *argv[]) {
     }
     
     client::Shell shell([&](const std::string &msg) -> void {
-        test_fn(msg);
+        // test_fn(msg);
         std::string recv_msg = send_request(stub, "sql", msg, ++log_id);
         printf("recv msg: %s\n", recv_msg.c_str());
+        json json_msg = json::parse(recv_msg);
+        if (json_msg.contains("data")) {
+            printf("row numbers: %d\n", (int)json_msg["data"].size());
+        }
     });
 
     shell.register_conf_cmd(
@@ -122,8 +141,32 @@ int main(int argc, char *argv[]) {
 
     shell.register_conf_cmd(
         "load",
-        [&](const std::string &msg) -> void {
-            std::string recv_msg = send_request(stub, "load", msg, ++log_id);
+        [&](const std::string &name) -> void {
+            std::string _name = name;
+            if (name.find('/') != std::string::npos)
+                _name = _name.substr(name.find_last_of('/') + 1);
+            std::ifstream f(name + ".tsv");
+            std::string line;
+            json data;
+            while (std::getline(f, line)) {
+                json row;
+                std::string value;
+                for (auto &c : line) {
+                    if (c != '\t')
+                        value += c;
+                    else {
+                        row.push_back(value);
+                        value = "";
+                    }
+                }
+                row.push_back(value);
+                data.push_back(row);
+            }
+            json msg = {
+                {"table", _name},
+                {"data", data}
+            };
+            std::string recv_msg = send_request(stub, "load", msg.dump(), ++log_id);
             printf("recv msg: %s\n", recv_msg.c_str());
         }
     );
